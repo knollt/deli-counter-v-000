e5701a4f7b0d5bd41cda51792ba38132262aed87
Tim Knoll <timothy.knoll@gmail.com>
2019-12-03 20:47:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -42,10 +42,10 @@ def line (line)
   end
 end  
 
-# katz_deli = []
 
 def take_a_number(katz_deli,name)
 
-  # katz_deli.push(name)
+katz_deli = []
+  katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end   
