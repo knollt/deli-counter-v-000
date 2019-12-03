a60821c04635d57dffe2ba33eee51eb6097f6246
Tim Knoll <timothy.knoll@gmail.com>
2019-12-03 20:47:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -47,5 +47,5 @@ katz_deli = []
 def take_a_number(katz_deli,name)
 
   # katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line.""
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end   
