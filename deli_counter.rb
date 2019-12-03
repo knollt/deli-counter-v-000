7be6796fbbae4d26718091a3901f9f15c9d060a2
Tim Knoll <timothy.knoll@gmail.com>
2019-12-03 20:53:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,54 +1,25 @@
-# Write your code here.
-# katz_deli = []
-
-# def take_a_number (katz_deli, name)
-#   katz_deli.push(name)
-#   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-# end
-
-
-# def line (line)
-#   curr_line = []
-#   if (line.length === 0)
-#     puts 'The line is currently empty.'
-#   else
-#     line.each.with_index(1) do |name, index|
-#       curr_line.push("#{index}. #{name}")
-#   end
-#     puts "The line is currently: #{curr_line.join(" ")}"
-#   end
-# end
-
-
-
-# def now_serving (line)
-#   if line.empty?
-#     puts "There is nobody waiting to be served!"
-#   else
-#     puts "Currently serving #{line[0]}."
-#     line.shift
-#   end
-# end
-
-
+Write your code here.
 katz_deli = []
 
-def take_a_number(katz_deli,name = 1)
+def take_a_number (katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-end 
+end
+
 
 def line (line)
   curr_line = []
-  if (line.length == 0)
-    puts "The line is currently empty."
+  if (line.length === 0)
+    puts 'The line is currently empty.'
   else
-    line.each.with_index(1) do |n, i|
-      curr_line.push("#{i}. #{n}")
-  end 
+    line.each.with_index(1) do |name, index|
+      curr_line.push("#{index}. #{name}")
+  end
     puts "The line is currently: #{curr_line.join(" ")}"
   end
-end  
+end
+
+
 
 def now_serving (line)
   if line.empty?
@@ -56,5 +27,7 @@ def now_serving (line)
   else
     puts "Currently serving #{line[0]}."
     line.shift
-  end  
-end   
+  end
+end
+
+
